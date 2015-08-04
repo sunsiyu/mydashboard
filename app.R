9f25ca18dc7c9b9b94b157a2a2c0603510dceab6
Siyu Sun <sunsiyu.tud@gmail.com>
2015-08-04 22:17:17 +0200

    first example.

--- app.R
+++ app.R
@@ -4,9 +4,28 @@ library(shinydashboard)
 ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(),
-  dashboardBody()
+  dashboardBody(
+    
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of obs:", 1, 100, 50)
+      )
+      
+    )  
+  )
 )
 
-server <- function(input, output){}
+server <- function(input, output){
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+  
+}
 
 shinyApp(ui, server)
